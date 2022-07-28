ce75ec1b6c78c6a02317698276f7590deb9b0531
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-27 23:27:24 -0300

    Fix lint

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -2,21 +2,30 @@ package com.trybe.acc.java;
 
 import java.util.Scanner;
 
+/** Imprime na tela o numero antecessor e o sucessor. */
 public class AntecessorSucessor {
 
-	public static void main(String[] args) {
-		// Ler a entrada de um número pelo console	
-		Scanner scan = new Scanner(System.in);
+  /** Ler a entrada de um número pelo console. */
+  public static void main(String[] args) {
+    StringBuilder stringBuilder = new StringBuilder();
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Digite um número:");
+    int numero = scan.nextInt();
+    int antecessor = numero - 1;
+    int sucessor = numero + 1;
+    
+    scan.close();
+        
+    StringBuilder frase = stringBuilder
+                                        .append("O número ")
+                                        .append(numero)
+                                        .append(" tem ")
+                                        .append(antecessor)
+                                        .append(" como seu antecessor e ")
+                                        .append(sucessor)
+                                        .append(" como seu sucessor.");
 
-		System.out.println("Digite um número:");
-		int numero = scan.nextInt();
-		int antecessor = numero - 1;
-		int sucessor = numero + 1;
-		
-				
-		// Imprime a saída no console. OBS: descomente a próxima linha
-		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
-
-	}
-
+    System.out.println(frase);
+  }
 }
